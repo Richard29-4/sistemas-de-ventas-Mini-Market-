10ba1d965d64da9d23f0a5467af8a207f5a81505
Richard <richyalejo226@gmail.com>
2025-10-12 15:11:52 -1200

    Update Descontable.java

--- src/App/Descontable.java
+++ src/App/Descontable.java
@@ -1,9 +1,10 @@
 package sistemaventas;
 public class descontable {
 
-// Interface (Program to an Interface)
+
 public interface Descontable {
     double aplicarDescuento(double porcentaje);
 }
     
 }
+
