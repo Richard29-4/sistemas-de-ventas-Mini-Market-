2b2609c2ec1aaaf9f2db7803806341ceafddd2f3
Richard <richyalejo226@gmail.com>
2025-10-14 08:07:24 -1200

    Delete Mini-Market directory

--- Mini-Market/src/App/Mini-Market/src/descontable.java
+++
@@ -1,9 +0,0 @@
-package sistemaventas;
-public class descontable {
-
-// Interface (Program to an Interface)
-public interface Descontable {
-    double aplicarDescuento(double porcentaje);
-}
-    
-}
