ed12d2a5ff0682935870a5b2a65378203f106a79
Richard <richyalejo226@gmail.com>
2025-11-17 14:33:38 -1200

    Update caja.java

--- src/App/caja.java
+++ src/App/caja.java
@@ -1,18 +1,21 @@
 package tu.paquete;
 
-public class Caja<T> {
+public class Caja <T> {
     private T contenido;
 
     public Caja(T contenido) {
         this.contenido = contenido;
     }
 
+    
     public T getContenido() {
         return contenido;
     }
 
+    
     public void setContenido(T contenido) {
         this.contenido = contenido;
     }
 }
+
 
