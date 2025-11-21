6a1f00c658ce13190fed3ccfe0b692efed94bc5c
Richard <richyalejo226@gmail.com>
2025-11-20 15:07:21 -1200

    Update Factura.java

--- src/App/Factura.java
+++ src/App/Factura.java
@@ -14,7 +14,7 @@ public class Factura {
     }
 
     public void mostrarFactura() {
-        System.out.println("\nro FACTURA #" + numeroFactura);
+        System.out.println("\Numero FACTURA #" + numeroFactura);
         cliente.mostrarInformacion();
         vendedor.mostrarInformacion();
         System.out.println("Total a pagar: S/ " + total);
