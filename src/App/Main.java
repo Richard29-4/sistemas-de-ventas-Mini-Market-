665e83dae730410a16430120f230894f42a09272
Richard <richyalejo226@gmail.com>
2025-11-21 15:22:46 -1200

    Update Main.java

--- src/App/Main.java
+++ src/App/Main.java
@@ -5,31 +5,24 @@ public class Main {
 
         SistemaVentas sistema = SistemaVentas.getInstancia();
 
-        // Mostrar productos cargados
         System.out.println("PRODUCTOS DISPONIBLES:");
         sistema.listarConLambda();
 
-        // Usar carrito
         Carrito carrito = new Carrito();
         carrito.agregarProducto(new Producto("Laptop", 3500));
         carrito.agregarProducto(new Producto("Mouse", 80));
         carrito.mostrarCarrito();
 
-        // ======= GUARDAR EN XML =======
         PersistenciaXML.guardarCarrito(carrito);
         System.out.println("Carrito guardado en carrito.xml");
 
-        // ======= CARGAR DESDE XML =======
         carrito = PersistenciaXML.cargarCarrito();
         System.out.println("Carrito cargado desde carrito.xml:");
         carrito.mostrarCarrito();
-        // ================================
 
-        // Cliente y vendedor
         Cliente cliente = new Cliente("Richard Alejo", "78945612", "Lima");
         Vendedor vendedor = new Vendedor("María Torres", "V001");
 
-        // Factura
         Factura factura = new Factura(1001, cliente, vendedor, carrito.calcularTotal());
         factura.mostrarFactura();
 
@@ -37,4 +30,5 @@ public class Main {
         pago.procesarPago();
     }
 }
+
 
